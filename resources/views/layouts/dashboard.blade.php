a38c8ed54e554f4ad0b9ecdce0963eda996f47bf
I Made Suardika <imade.suardika99@yahoo.com>
2021-05-31 15:41:33 +0800

    add template

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.master')
+
+@section('title', 'Dashboard | FindPeken')
+
+@section('content')
+<section class="content">
+    <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>150</h3>
+
+                        <p>Pasar</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-store"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>10</h3>
+                        <p>Admin</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
+</section>
+@endsection()
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    <div><strong>Copyright &copy; {{date('Y')}} <a href="#">FindPeken</a>. All rights
+            reserved.</strong></div>
+</footer>
